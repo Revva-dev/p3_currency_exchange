0b71cef379fb298437f5ff3e41d297c54022ee46
revva-dev <om1357@mail.ru>
2026-03-14 12:53:49 +0300

    add a new field CharCod

--- src/main/java/ru/skillbox/currency/exchange/dto/CurrencyDto.java
+++ src/main/java/ru/skillbox/currency/exchange/dto/CurrencyDto.java
@@ -1,22 +1,52 @@
 package ru.skillbox.currency.exchange.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonView;
+import lombok.*;
 
-@Getter
-@Setter
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.xml.bind.annotation.*;
+
+@Data
+@XmlAccessorType(XmlAccessType.FIELD)
 @NoArgsConstructor
 @AllArgsConstructor
 public class CurrencyDto {
+
+    public interface GetAllCurrencies {
+    }
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @NotNull
+    @Max(1000000000)
+    private Long count;
+
     private Long id;
 
+    @JsonView(GetAllCurrencies.class)
+    @XmlElement(name = "Name")
     private String name;
 
+    @XmlElement(name = "Nominal")
     private Long nominal;
 
-    private Double value;
+    @JsonView(GetAllCurrencies.class)
+    @XmlElement(name = "Value")
+    private String value;
 
+    @XmlElement(name = "NumCode")
+    @Min(2)
+    @Max(3)
+    @NotNull
     private Long isoNumCode;
+
+    @XmlElement(name = "CharCode")
+    private String charCode;
+
+    @Override
+    public String toString() {
+        return name + " "  + value + " " + " " + isoNumCode;
+    }
 }
